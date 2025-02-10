495bd3b6edd676173b1c582a8ce4b7cde7127bc7
muhammedkamilbarad <muhammedkamilbarad@gmail.com>
2025-02-10 13:26:38 +0300

    Add User Api

---
+++ Server/app/Services/UserService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Repositories\UserRepository;
+
+
+class UserService
+{
+    protected $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function getAllUsers(): Collection
+    {
+        return $this->userRepository->getAll();
+    }
+
+    public function getUserById(int $id): ?Model
+    {
+        try
+        {
+            return $this->userRepository->getById($id);
+        }
+        catch (ModelNotFoundException $e)
+        {
+            return null;
+        }
+    }
+
+    public function createUser(array $data): Model
+    {
+        return $this->userRepository->create($data);
+    }
+
+    public function updateUser(Model $user, array $data): Model
+    {
+        return $this->userRepository->update($user, $data);
+    }
+
+    public function deleteUser(Model $user)
+    {
+        $this->userRepository->delete($user);
+    }
+}
